48f4b26e68f220ba658cb690a3e80462a90636f7
Deep <debojeetkarmakar2020@gmail.com>
2023-09-06 22:13:55 +0530

    my first comit

---
+++ toggle_switch.js
@@ -0,0 +1,11 @@
+// script.js
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+});
